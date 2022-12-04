eaacc1750a214a710b3c2cb5f626d728569d7655
Rahul Awasthi <awasthir314@gmail.com>
2022-12-04 21:02:38 +0530

    Added Basic navigation UI

---
+++ app/src/main/java/com/awasthir/aapanam/activities/PaymentActivity.java
@@ -0,0 +1,33 @@
+package com.awasthir.aapanam.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.awasthir.aapanam.databinding.ActivityPaymentBinding;
+import com.awasthir.aapanam.utils.Constants;
+
+public class PaymentActivity extends AppCompatActivity {
+
+    ActivityPaymentBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityPaymentBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        String orderCode = getIntent().getStringExtra("orderCode");
+
+        binding.webview.setMixedContentAllowed(true);
+        binding.webview.loadUrl(Constants.PAYMENT_URL + orderCode);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return super.onSupportNavigateUp();
+    }
+}
---
+++ app/src/main/java/com/awasthir/aapanam/adapters/CategoryAdapter.java
@@ -0,0 +1,76 @@
+package com.awasthir.aapanam.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.awasthir.aapanam.R;
+import com.awasthir.aapanam.activities.CategoryActivity;
+import com.awasthir.aapanam.activities.MainActivity;
+import com.awasthir.aapanam.databinding.ItemCategoriesBinding;
+import com.awasthir.aapanam.model.Category;
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder> {
+
+    Context context;
+    ArrayList<Category> categories;
+
+    public CategoryAdapter(Context context, ArrayList<Category> categories) {
+        this.context = context;
+        this.categories = categories;
+    }
+
+
+
+    @NonNull
+    @Override
+    public CategoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new CategoryViewHolder(LayoutInflater.from(context).inflate(R.layout.item_categories, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryViewHolder holder, int position) {
+        Category category = categories.get(position);
+        holder.binding.label.setText(Html.fromHtml(category.getName()));
+        Glide.with(context)
+                .load(category.getIcon())
+                .into(holder.binding.image);
+
+        holder.binding.image.setBackgroundColor(Color.parseColor(category.getColor()));
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, CategoryActivity.class);
+                intent.putExtra("catId", category.getId());
+                intent.putExtra("categoryName", category.getName());
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return categories.size();
+    }
+
+    public class CategoryViewHolder extends RecyclerView.ViewHolder {
+        ItemCategoriesBinding binding;
+
+        public CategoryViewHolder(@NonNull View itemView) {
+            super(itemView);
+            binding = ItemCategoriesBinding.bind(itemView);
+        }
+    }
+}
